9885e3eb33e646da049870e548fe304bdced4972
Francesco Sullo <sullof@gmail.com>
2024-01-03 16:52:54 -0800

    Minor

---
+++ dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source .env && docker run -it --rm \
+  --name api-cruna-cc-dev \
+  -p 3210 \
+  -v $PWD:/usr/src/app \
+  -v $PWD/log:/var/log/allertatore \
+  -e NODE_ENV=development \
+  -e VIRTUAL_HOST=$SERVER.local \
+  -w /usr/src/app node:16 npm run start-prod
