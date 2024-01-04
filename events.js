2e4a3bec858ab1c705604f1e16dcd571955ae78c
Francesco Sullo <sullof@gmail.com>
2024-01-03 17:32:31 -0800

    minor changes

--- events.js
+++ events.js
@@ -3,7 +3,7 @@ const events = {
         {
             title: 'Call with Andy and Mack',
             date: '2024-01-03',
-            time: '17:35'
+            time: '17:50'
         },
         {
             title: 'Call with Federico',
