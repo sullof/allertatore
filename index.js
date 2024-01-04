dff23d2f9e179ee0e30d35ae1a9447eb846b8624
Francesco Sullo <sullof@gmail.com>
2024-01-03 17:34:11 -0800

    minor changes

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ app.listen(3210, () => {
 });
 
 function makeReminderCall(event) {
+    console.log("Calling in relation to " + event.title);
     let eventDetails = `You have an event titled ${event.title} at ${event.date}, ${event.time}.`; // Customize this line as needed
     client.calls
         .create({
