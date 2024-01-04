9a36dac5d33bbf5c7a7a866b07c490d58d6e2394
Francesco Sullo <sullof@gmail.com>
2024-01-04 13:52:45 -0800

    New version more powerful

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const moment = require('moment-timezone');
 const express = require('express');
 const app = express();
 
@@ -7,11 +8,14 @@ const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUT
 
 // TODO, this file is hidden, look at sample.env.events for an example
 const events = require('./events');
+
+const now = new Date();
+const oneMinute = 60 * 1000;
+
 for (let event of events.calendar) {
-    const combinedDateTime = `${event.date}T${event.time}:00`;
-    let date = new Date(combinedDateTime);
-    date.setHours(date.getHours() + 8);
-    event.utcDateTime = date.toISOString();
+    const date = moment.tz(event.date, event.timezone);
+    const utcDate = date.clone().tz("UTC");
+    event.utcDateTime = utcDate.toDate().toISOString();
 }
 
 // Express server to handle dynamic TwiML responses
@@ -43,14 +47,12 @@ app.get("/", (req, res) => {
     });
 })
 
-app.listen(3210, () => {
-    console.log('Express server running on port 3210');
-});
-
-function makeReminderCall(event) {
+function makeReminderCall(event, when) {
     console.log("Calling in relation to");
     console.log(event);
-    let eventDetails = `You have an event titled ${event.title} at ${event.date}, ${event.time}.`; // Customize this line as needed
+    let minutes = Math.floor((when.getTime() - Date.now()) / 60000);
+    const day = new Date(event.utcDateTime);
+    let eventDetails = `You have an event titled "${event.title}" on ${day.toLocaleString('en-US', { timeZone: event.timezone })}. Let me repeat. You have an event titled "${event.title}" in ${minutes} minutes`; // Customize this line as needed
     client.calls
         .create({
             url: `https://${process.env.SERVER}/twiml?eventDetails=${encodeURIComponent(eventDetails)}`, // Replace with your server address
@@ -61,22 +63,31 @@ function makeReminderCall(event) {
         .catch(error => console.error(error));
 }
 
-const min15 = 15 * 60 * 1000;
-
-let now = Date.now();
-
-for (let event of events.calendar) {
-    let when = new Date(event.utcDateTime);
-    let ts = when.getTime();
-    if (ts - min15 > now) {
-        (function (event, when) {
-            when.setHours(when.getHours() - 8);
-            console.log("Scheduling " + event.title + " at " + when.toString());
-            setTimeout(() => {
-                makeReminderCall(event);
-            }, ts - now - min15);
-        })(event, when);
-    }
+function schedule(ts, event, when, offset) {
+    when.setHours(when.getHours() - 8);
+    setTimeout(() => {
+        makeReminderCall(event, when);
+    }, ts - now - offset);
 }
 
-console.log("Server started");
+
+app.listen(3210, () => {
+    console.log('Express server running on port 3210');
+    for (let event of events.calendar) {
+        let when = new Date(event.utcDateTime);
+        let ts = when.getTime();
+        let offset = [15 * oneMinute, 2 * oneMinute];
+        if (event.offset) {
+            offset = event.offset.map(e => e * oneMinute);
+        }
+        const at = when.toLocaleString('en-US', { timeZone: event.timezone });
+        for (let o of offset) {
+            if (ts - o > now) {
+                console.log("\nScheduling: " + event.title + "\nat " + at + ", offset " + o/60000 + " minutes");
+                schedule(ts, event, when, o);
+            }
+        }
+    }
+});
+
+
